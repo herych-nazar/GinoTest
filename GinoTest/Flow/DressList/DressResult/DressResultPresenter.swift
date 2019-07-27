b89f56dc80e13142db647da6046ea4ef647b4280
Nazar Herych <herych.nazar@gmail.com>
2019-07-27 12:10:20 +0300

    Created dependency for DressFilter, SizeCalculator, DressResult

---
+++ GinoTest/Flow/DressList/DressResult/DressResultPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  DressResultPresenter.swift
+//  GinoTest
+//
+//  Created by Nazar Herych on 7/27/19.
+//  Copyright © 2019 Nazar Herych. All rights reserved.
+//
+
+import Foundation
---
+++ GinoTest/Flow/DressList/Filter/DressFilterPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  DressFilterPresenter.swift
+//  GinoTest
+//
+//  Created by Nazar Herych on 7/27/19.
+//  Copyright © 2019 Nazar Herych. All rights reserved.
+//
+
+import Foundation
